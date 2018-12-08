da44888ee680acc52f76f80f55b94c01b47b3833
HoweChen <yuhao.howe.chen@gmail.com>
2018-12-08 20:05:49 +0800

    update with mtcnn example

---
+++ face_recognition/mtcnn_example.py
@@ -0,0 +1,28 @@
+import cv2
+from mtcnn.mtcnn import MTCNN
+
+detector = MTCNN()
+
+image = cv2.imread("examples/obama.jpg")
+result = detector.detect_faces(image)
+
+# Result is an array with all the bounding boxes detected. We know that for 'ivan.jpg' there is only one.
+bounding_box = result[0]['box']
+keypoints = result[0]['keypoints']
+
+cv2.rectangle(image,
+              (bounding_box[0], bounding_box[1]),
+              (bounding_box[0]+bounding_box[2], bounding_box[1] + bounding_box[3]),
+              (0,155,255),
+              2)
+
+cv2.circle(image,(keypoints['left_eye']), 2, (0,155,255), 2)
+cv2.circle(image,(keypoints['right_eye']), 2, (0,155,255), 2)
+cv2.circle(image,(keypoints['nose']), 2, (0,155,255), 2)
+cv2.circle(image,(keypoints['mouth_left']), 2, (0,155,255), 2)
+cv2.circle(image,(keypoints['mouth_right']), 2, (0,155,255), 2)
+
+cv2.imshow("result", image)
+# Hit 'q' on the keyboard to quit!
+cv2.waitKey(0)
+cv2.destroyAllWindows()
