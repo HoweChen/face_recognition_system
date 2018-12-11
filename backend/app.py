ff302bf57518d146f2cb7dc49a40212b2c42b822
HoweChen <yuhao.howe.chen@gmail.com>
2018-12-11 16:47:41 +0800

    update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, Response, render_template
 
 app = Flask(__name__)
 
@@ -6,6 +6,10 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+@app.route('/camera')
+def camera():
+    return
 
 
 if __name__ == '__main__':
