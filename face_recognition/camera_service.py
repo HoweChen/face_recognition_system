14ca4bd1a342907526413f47e2e3d7ba331b4106
HoweChen <yuhao.howe.chen@gmail.com>
2018-12-11 20:01:53 +0800

    refactor the camera service code

--- face_recognition/camera_service.py
+++ face_recognition/camera_service.py
@@ -29,18 +29,26 @@ def get_random_RGB_color():
 class Instance:
     def __init__(self, mode="HOG"):
         # connect to the redis service
+        self.mode = mode
         self.redis_pool = redis.ConnectionPool()
         self.r = redis.Redis(connection_pool=self.redis_pool)
         self.detector = MTCNN() if mode == "MTCNN" else None
-        self.process_this_frame = True
 
         self.data_validation()
 
         # Initialize some variables
-        face_locations = []
-        face_encodings = []
+        self.face_locations = []
+        self.face_encodings = []
+        self.face_names = []
+        self.best_point_list = []
+
+        self.serve()
+
+    def serve(self):
 
         video_capture = cv2.VideoCapture(0)
+
+        process_this_frame = True
 
         while True:
             # Grab a single frame of video
@@ -53,88 +61,25 @@ class Instance:
             rgb_small_frame = small_frame[:, :, ::-1]
 
             # Only process every other frame of video to save time
-            if self.process_this_frame:
-                # Find all the faces and face encodings in the current frame of video
-                # face_locations = face_recognition.face_locations(rgb_small_frame)
-                if mode == "MTCNN" and self.detector is not None:
-                    try:
-                        detect_result = self.detector.detect_faces(rgb_small_frame)[0]["box"]
-                    except IndexError as e:
-                        # which means the detector doesn't find the faces
+            if process_this_frame:
+                self.face_detection(rgb_small_frame)
+                self.face_matching()
 
-                        # Display the resulting image
-                        cv2.imshow('Video', frame)
-
-                        # Hit 'q' on the keyboard to quit!
-                        if cv2.waitKey(1) & 0xFF == ord('q'):
-                            break
-                        continue
-
-                    # the detector does find the faces
-                    face_locations = [tuple(
-                            [detect_result[1], detect_result[0] + detect_result[2],
-                             detect_result[1] + detect_result[-1],
-                             detect_result[0]])]
-                    face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
-
-                if mode == "HOG":
-                    face_locations = face_recognition.face_locations(rgb_small_frame)
-                    face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
-
-                face_names = []
-                best_point_list = []
-
-                for face_encoding in face_encodings:
-                    # See if the face is a match for the known face(s)
-                    true_or_false, points = face_recognition.compare_faces(
-                            list(map(lambda x: np.frombuffer(x), self.r.lrange("known_face_encodings", 0, -1))),
-                            face_encoding,
-                            tolerance=0.4)
-                    name = "Unknown"
-                    best_point = None
-
-                    # If a match was found in known_face_encodings, just use the first one.
-                    if True in true_or_false and len(true_or_false) == len(points):
-                        best_point = min(points)
-                        winner_index = points.index(best_point)
-                        print(winner_index)
-                        name = self.r.lrange("known_face_names", 0, -1)[winner_index].decode("utf-8")
-                    best_point_list.append(best_point)
-                    face_names.append(name)
-
-            self.process_this_frame = not self.process_this_frame
+            process_this_frame = not process_this_frame
 
             # Display the results
-            for (top, right, bottom, left), temp_name, temp_best_point in zip(face_locations, face_names,
-                                                                              best_point_list):
-                # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-                top *= 4
-                right *= 4
-                bottom *= 4
-                left *= 4
-
-                # Draw a box around the face
-                cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-
-                # Draw a label with a name below the face
-                cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-                font = cv2.FONT_HERSHEY_DUPLEX
-                if temp_best_point is not None:
-                    cv2.putText(frame, f"{temp_name}: {temp_best_point:.3f}%", (left + 6, bottom - 6), font, 0.9,
-                                (255, 255, 255), 1)
-                else:
-                    cv2.putText(frame, f"{temp_name}", (left + 6, bottom - 6), font, 0.9,
-                                (255, 255, 255), 1)
+            self.render_boxes(frame)
 
             # Hit '1' on the keyboard to input the new face into the database!
             if cv2.waitKey(1) & 0xFF == ord('1'):
 
                 try:
-                    self.r.rpushx("known_face_encodings", face_encodings[0].tostring())
+                    self.r.rpushx("known_face_encodings", self.face_encodings[0].tostring())
                     self.r.rpushx("known_face_names", "Yuhao Chen")
                     print("Success with:", end=" ")
                     print(self.r.lrange("known_face_names", 0, -1))
                 except Exception as e:
+                    print(e)
                     pass
 
             # Display the resulting image
@@ -147,6 +92,73 @@ class Instance:
         # Release handle to the webcam
         video_capture.release()
         cv2.destroyAllWindows()
+
+    def face_detection(self, input_frame):
+        # Find all the faces and face encodings in the current frame of video
+        if self.mode == "MTCNN" and self.detector is not None:
+            try:
+                detect_result = self.detector.detect_faces(input_frame)[0]["box"]
+            except IndexError:
+                # which means the detector doesn't find the faces, then directly show the image without box
+                detect_result = None
+
+            if detect_result is not None:
+                # the detector does find the faces
+                self.face_locations = [tuple(
+                        [detect_result[1], detect_result[0] + detect_result[2],
+                         detect_result[1] + detect_result[-1],
+                         detect_result[0]])]
+                self.face_encodings = face_recognition.face_encodings(input_frame, self.face_locations)
+
+        if self.mode == "HOG":
+            self.face_locations = face_recognition.face_locations(input_frame)
+            self.face_encodings = face_recognition.face_encodings(input_frame, self.face_locations)
+
+    def face_matching(self):
+
+        self.face_names = []
+        self.best_point_list = []
+
+        for face_encoding in self.face_encodings:
+            # See if the face is a match for the known face(s)
+            true_or_false, points = face_recognition.compare_faces(
+                    list(map(lambda x: np.frombuffer(x), self.r.lrange("known_face_encodings", 0, -1))),
+                    face_encoding,
+                    tolerance=0.4)
+            name = "Unknown"
+            best_point = None
+
+            # If a match was found in known_face_encodings, just use the first one.
+            if True in true_or_false and len(true_or_false) == len(points):
+                best_point = min(points)
+                winner_index = points.index(best_point)
+                print(winner_index)
+                name = self.r.lrange("known_face_names", 0, -1)[winner_index].decode("utf-8")
+            self.face_names.append(name)
+            self.best_point_list.append(best_point)
+
+    def render_boxes(self, frame):
+        # Display the results
+        for (top, right, bottom, left), temp_name, temp_best_point in zip(self.face_locations, self.face_names,
+                                                                          self.best_point_list):
+            # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+            top *= 4
+            right *= 4
+            bottom *= 4
+            left *= 4
+
+            # Draw a box around the face
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+            # Draw a label with a name below the face
+            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            if temp_best_point is not None:
+                cv2.putText(frame, f"{temp_name}: {temp_best_point:.3f}%", (left + 6, bottom - 6), font, 0.9,
+                            (255, 255, 255), 1)
+            else:
+                cv2.putText(frame, f"{temp_name}", (left + 6, bottom - 6), font, 0.9,
+                            (255, 255, 255), 1)
 
     def data_validation(self):
         # Load a sample picture and learn how to recognize it.
@@ -168,5 +180,5 @@ class Instance:
 
 
 if __name__ == '__main__':
-    instance = Instance(mode="HOG")
-    # instance = Instance(mode="MTCNN")
+    # instance = Instance(mode="HOG")
+    instance = Instance(mode="MTCNN")
