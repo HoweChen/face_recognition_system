ef25f0f211ee2c4160c4fb54632c5f8f3098e684
HoweChen <yuhao.howe.chen@gmail.com>
2018-12-23 16:56:47 +0800

    update flask app with socket.io

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, Response
 import cv2
 from flask_camera_service import Camera
-import subprocess
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
 vc = cv2.VideoCapture(0)
+socketio = SocketIO(app)
 
 
 @app.route('/')
@@ -23,7 +24,7 @@ def gen():
     #            b'Content-Type: image/jpeg\r\n\r\n' + cv2.imencode(".jpg",frame)[1].tostring() + b'\r\n')
     for frame in camera_instance.serve():
         yield (b'--frame\r\n'
-                          b'Content-Type: image/jpeg\r\n\r\n' + cv2.imencode(".jpg",frame)[1].tostring() + b'\r\n')
+               b'Content-Type: image/jpeg\r\n\r\n' + cv2.imencode(".jpg", frame)[1].tostring() + b'\r\n')
 
 
 @app.route('/video_feed')
@@ -33,5 +34,26 @@ def video_feed():
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
+@socketio.on("message")
+def handle_message(message):
+    print(message)
+
+
+@socketio.on("image")
+def handle_image(image: str) -> None:
+    print(image)
+
+@socketio.on("connect")
+def handle_connect():
+    print("\nClient Connected")
+
+
+@socketio.on("disconnect")
+def handle_disconnect():
+    print("\nClient disconnected")
+
+
 if __name__ == '__main__':
-    app.run(debug=True, threaded=True)
+    # app.run(debug=True, threaded=True)
+    # The flask_socketio encapsulates the start up of the web server
+    socketio.run(app)
