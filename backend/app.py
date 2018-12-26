0340925c7a7f179ecd74b497041d66eafc067b22
HoweChen <yuhao.howe.chen@gmail.com>
2018-12-26 23:51:35 +0800

    update on join, leave the room and create the instance function

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, Response
 import cv2
 from flask_camera_service import Camera
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, join_room, leave_room, send
+import redis
 
 app = Flask(__name__)
 vc = cv2.VideoCapture(0)
 socketio = SocketIO(app)
+redis_pool = redis.ConnectionPool()
 
 
 @app.route('/')
@@ -39,13 +41,45 @@ def handle_message(message):
     print(message)
 
 
+@socketio.on("join")
+def on_join(data):
+    user_name = data["userName"]
+    room = data["room"]
+    print(room)
+    join_room(room, sid=room)  # in here the room string and request.sid are the same
+    send(f"{user_name} has entered the room: {room}.", room=room)
+    print(socketio.room_users)
+
+
+@socketio.on("leave")
+def on_leave(data):
+    user_name = data["userName"]
+    room = data["room"]
+    leave_room(room, sid=room)
+    send(f"{user_name} has left the room: {room}.", room=room)
+
+
+@socketio.on("create_instance")
+def handle_test(data):
+    user_name = data["userName"]
+    room = data["userName"]
+    camera_instance = Camera(mode="MTCNN")
+    send(f"We create for user: {user_name} an instance of camera: {id(camera_instance)}", room=room)
+
+
 @socketio.on("image")
 def handle_image(image: str) -> None:
-    print(image)
+    # print(image)
+    # print(id(camera_instance))
+    r = redis.Redis(connection_pool=redis_pool)
+
+
 
 @socketio.on("connect")
 def handle_connect():
     print("\nClient Connected")
+    # camera_instance = Camera(mode="MTCNN")
+    # print(camera_instance)
 
 
 @socketio.on("disconnect")
