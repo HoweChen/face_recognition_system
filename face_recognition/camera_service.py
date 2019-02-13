f294e8cb6bebf549b6f53b1e3b8333cd650b7512
HoweChen <yuhao.howe.chen@gmail.com>
2019-02-13 16:45:26 +0800

    remove useless comment

--- face_recognition/camera_service.py
+++ face_recognition/camera_service.py
@@ -12,14 +12,6 @@ class ImagePath(Enum):
     BIDEN_IMAGE_FILE = "examples/biden.jpg"
 
 
-"""
-This detector can only scan one person with a better result
-
-A better result means that for one person, if you cover your mouth, the detector could still detect your face.
-
-If you want the multiple person detection and recognition, please use HOG version instead
-
-"""
 
 
 class Instance:
