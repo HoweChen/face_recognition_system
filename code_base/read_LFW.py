115a0f3578beea0abe3bb721ad2d5bb7fd493124
HoweChen <yuhao.howe.chen@gmail.com>
2019-04-18 11:32:58 +0800

    upgrade read_LFW

--- code_base/read_LFW.py
+++ code_base/read_LFW.py
@@ -1,16 +1,28 @@
 import pathlib
 import redis
 from code_base.camera_service import Instance
+from multiprocessing import Pool
 
 folders = pathlib.Path("/Users/howechen/Dropbox/Lab/dataset/CV/lfw")
 
-if __name__ == '__main__':
-    ins = Instance(mode="MTCNN", f_e_m="NORMAL")
 
+def register_to_db(file, file_name):
+    ins = Instance()
+    try:
+        ins.image_to_db(filepath=str(file.absolute()), name=str(file_name), image_size="SMALL",
+                             num_jitters=1)
+    except Exception as e:
+        raise e
+
+
+if __name__ == '__main__':
+    # ins = Instance(mode="MTCNN", f_e_m="NORMAL")
+    # TODO: 实现多进程，现在只是主进程把文件放进多进程里，但是没有用
     redis_pool = redis.ConnectionPool()
     r = redis.Redis(connection_pool=redis_pool)
     count = 0
     failed_count = 0
+    p = Pool(processes=None)
     for sub_folder in folders.iterdir():
         if sub_folder.is_dir():
             sub_folder = pathlib.Path(sub_folder)
@@ -19,8 +31,9 @@ if __name__ == '__main__':
                     count += 1
                     try:
                         file_name = file.name.replace(".jpg", "")
-                        ins.image_to_db(filepath=str(file.absolute()), name=str(file_name), image_size="SMALL",
-                                        num_jitters=1)
+                        # ins.image_to_db(filepath=str(file.absolute()), name=str(file_name), image_size="SMALL",
+                        #                 num_jitters=1)
+                        p.apply(register_to_db, args=(file, file_name,))
                     except Exception as e:
                         print(e)
                         failed_count += 1
